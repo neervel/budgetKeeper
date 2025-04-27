762100703a061895f01c45bfd4891d894df9b13c
Kirill Lihopoy <kirill.likhopoy@1win.pro>
2025-04-27 16:00:58 +0500

    add fix for empty currency rate

--- src/utils/convertToKZT.ts
+++ src/utils/convertToKZT.ts
@@ -1,11 +1,15 @@
 import { currencyRates } from '../config/currencyRates';
+import { log } from './logger';
 
 export const convertToKZT = (expenses: { amount: number, currency: string }[]) => {
+  log.info(expenses, 'Expenses for convert');
+
   return expenses.map((expense: any) => {
     let convertedAmount = expense.amount;
 
     if (expense.currency !== 'KZT') {
-      convertedAmount = Math.round(convertedAmount * currencyRates[expense.currency as keyof typeof currencyRates] / 10) * 10
+      const currencyRate = currencyRates[expense.currency as keyof typeof currencyRates] || 1;
+      convertedAmount = Math.round(convertedAmount * currencyRate / 10) * 10
     }
 
     delete expense.currency;
